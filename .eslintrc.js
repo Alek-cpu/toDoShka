c99f93794cd7f1e24395791a0d2cc7855c898736
Alexander Pryanikov <sanyapryanikov795@gmail.com>
2025-11-05 21:03:29 +0400

    chore: initial project toDoShka

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: ['plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  settings: { react: { version: 'detect' } },
+  rules: {}
+};
